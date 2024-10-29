51287eba6569ae2e312d231c26a7b25a0e9d87ef
kenta-nemo <kenta@nemonetwork.org>
2024-10-29 14:25:24 +0100

    fix: min provider count

--- providers/apis/dydx/parse.go
+++ providers/apis/dydx/parse.go
@@ -97,7 +97,7 @@ func CreateTickerFromMarket(market dydxtypes.MarketParam) (mmtypes.Ticker, error
 	t := mmtypes.Ticker{
 		CurrencyPair:     cp,
 		Decimals:         uint64(market.Exponent * -1),
-		MinProviderCount: uint64(market.MinExchanges),
+		MinProviderCount: 1,
 		Enabled:          true,
 	}
 
